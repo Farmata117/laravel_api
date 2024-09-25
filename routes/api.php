8cd4948941d1ec85f3b4a965495cf4b6b9f50340
unknown <farmatafaye22@gmail.com>
2024-09-25 20:45:44 +0000

    first commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\AuthController;
+
+
+// Public routes
+
+Route::post('/register',[AuthController::Class ,'register']);
+Route::post('/login',[AuthController::Class ,'login']);
+
+
+// Protected routes
+Route::group(['middleware' =>['auth:sanctum']], function(){
+
+    // User
+    Route::get('/user',[AuthController::Class ,'user']);
+    Route::post('/logout',[AuthController::Class ,'logout']);
+    Route::post('/admin/assign-role', [AuthController::class, 'assignRole']);
+
+});
